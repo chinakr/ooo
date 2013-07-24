9845d6f1397049befb96c6c941059abd21065075
chinakr <chinakr@gmail.com>
2013-07-24 17:16:35 +0800

    Limit access to group admin.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :authorize
+  protected
+    def authorize
+      @user = User.find(session[:user_id])
+      @admin = Group.find_by_code_name('admin')
+      unless @user.groups.include?(@admin)
+        session[:user_id] = nil
+        redirect_to login_url, alert: t('messages.permission_denied')
+      end
+    end
 end
