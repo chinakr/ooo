e001db371d13602bd20f17ab5e9bbf8463efb761
chinakr <chinakr@gmail.com>
2013-07-29 09:15:10 +0800

    Redirect to login page when not logging.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,14 @@ class ApplicationController < ActionController::Base
   before_filter :authorize
   protected
     def authorize
-      @user = User.find(session[:user_id])
-      @admin = Group.find_by_code_name('admin')
-      unless @user.groups.include?(@admin)
-        session[:user_id] = nil
+      begin
+        @user = User.find(session[:user_id])
+        @admin = Group.find_by_code_name('admin')
+        unless @user.groups.include?(@admin)
+          session[:user_id] = nil
+          redirect_to login_url, alert: t('messages.permission_denied')
+        end
+      rescue
         redirect_to login_url, alert: t('messages.permission_denied')
       end
     end
